a9049360435bd2cd7297ec578bf5e1d0200123a9
Pranshu Dave <pranshudave@gmail.com>
2018-03-19 23:54:11 +0530

    Try message loop fix

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from fbmq import Page
 from flask import Flask, request
 
-import CONFIG
+from CONFIG import CONFIG
 from helper import *
 
 chatterbot = ChatBot("Training Example")
@@ -100,106 +100,111 @@ def social_media_handle(message, sender_id):
 
 @page.handle_message
 def message_handler(event):
-    sender_id = event.sender_id
-    message = event.message_text
+    try:
+        sender_id = event.sender_id
+        message = event.message_text
 
-    if message is None:
-        return
-
-    message_handled = 0
-    categories_list = ['info', 'categories', 'category', 'details']
-    reach_us_list = ['navigate', 'reach', 'map', 'bvm', 'birla', 'vishvakarma', 'mahavidyalaya', 'college']
-
-    # Handle Social Media Queries
-    if social_media_handle(message, sender_id) == 1:
-        return
-    if ('when' in message.lower() and 'udaan' in message.lower()) or message.lower() == 'when':
-        page.send(sender_id, '2nd, 3rd and 4th April, 2018 :)')
-        page.send(sender_id, 'We look forward to seeing you there')
-        return
-    if ('where' in message.lower() and 'udaan' in message.lower()) or message.lower() == 'where':
-        click_persistent_menu(payload='PMENU_' + 'map', event=event)
-        return
-    if message.lower() in 'hey hello hi there hey there':
-        page.send(sender_id, 'Hey there! How you doing?')
-        return
-    # For handling inputs related to tech events
-    for tech_dept in data[2]:
-        for tech_event in tech_dept:
-            if tech_event.lower() in message.lower():
-                callback_clicked_tech(payload='TECH_' + tech_event, event=event, data=data, raw_data=raw_data,
-                                      page=page)
-                message_handled = 1
-
-    # For handling inputs related to other events
-    for z, category in enumerate(data[1], 0):
-        if z == data[0].index('technical'):  # Ignore technical events
-            continue
-        for other_event in category:
-            if other_event.lower() in message.lower():
-                callback_clicked_other(payload='OTHER_' + other_event, event=event, data=data, raw_data=raw_data,
-                                       page=page)
-
-                message_handled = 1
-
-    if message_handled == 1:
-        return
-
-    # For displaying department events
-    for dept_idx, dept in enumerate(data[1][data[0].index('technical')], 0):
-        dept_list = dept.split('/')
-        dept_list.append(raw_data['technical'][dept_idx]['alis'])
-        if dept == 'ec/el':
-            dept_list = ['electronics', 'communication']
-        if dept == 'computer/it':
-            dept_list = ['computer', 'information technology']
-
-        for mdept in dept_list:
-            if mdept.lower() in message.lower():
-                callback_picked_dept(payload='PICK_' + dept, event=event, data=data, raw_data=raw_data, page=page)
-                return
-
-    # For displaying info related to categories
-    for category in data[0]:
-        if category.lower() in message.lower():
-            if category.lower() == 'technical' and 'non' in message.lower():
-                callback_picked_category(payload='CATEGORY__non-technical', event=event, data=data, raw_data=raw_data,
-                                         page=page)
-            else:
-                callback_picked_category(payload='CATEGORY__' + category, event=event, data=data, raw_data=raw_data,
-                                         page=page)
+        if message is None:
             return
-    non_tech_keywords = ['non-tech', 'nontech']
-    for non_tech_keyword in non_tech_keywords:
-        if non_tech_keyword in message.lower():
-            callback_picked_category(payload='CATEGORY__non-technical', event=event, data=data, raw_data=raw_data,
-                                     page=page)
-            return
-    if 'tech' in message.lower():
-        callback_picked_category(payload='CATEGORY__technical', event=event, data=data, raw_data=raw_data,
-                                 page=page)
-        return
 
-    # Reach us
-    for keyword in reach_us_list:
-        if keyword.lower() in message.lower():
+        message_handled = 0
+        categories_list = ['info', 'categories', 'category', 'details']
+        reach_us_list = ['navigate', 'reach', 'map', 'bvm', 'birla', 'vishvakarma', 'mahavidyalaya', 'college']
+
+        # Handle Social Media Queries
+        if social_media_handle(message, sender_id) == 1:
+            return
+        if ('when' in message.lower() and 'udaan' in message.lower()) or message.lower() == 'when':
+            page.send(sender_id, '2nd, 3rd and 4th April, 2018 :)')
+            page.send(sender_id, 'We look forward to seeing you there')
+            return
+        if ('where' in message.lower() and 'udaan' in message.lower()) or message.lower() == 'where':
             click_persistent_menu(payload='PMENU_' + 'map', event=event)
             return
-
-    # Level 0 Menu
-    for keyword in categories_list:
-        if keyword.lower() in message.lower():
-            click_persistent_menu(payload='PMENU_' + 'Information', event=event)
+        if message.lower() in 'hey hello hi there hey there':
+            page.send(sender_id, 'Hey there! How you doing?')
             return
-    if 'fine' in message.lower():
-        page.send(sender_id, 'Good')
-        return
-    bot_input = chatterbot.get_response(message)
-    if 'do you feel' in str(bot_input).lower():
-        page.send(sender_id, 'Good')
-    else:
-        print(bot_input)
-        page.send(sender_id, str(bot_input))
+        # For handling inputs related to tech events
+        for tech_dept in data[2]:
+            for tech_event in tech_dept:
+                if tech_event.lower() in message.lower():
+                    callback_clicked_tech(payload='TECH_' + tech_event, event=event, data=data, raw_data=raw_data,
+                                          page=page)
+                    message_handled = 1
+
+        # For handling inputs related to other events
+        for z, category in enumerate(data[1], 0):
+            if z == data[0].index('technical'):  # Ignore technical events
+                continue
+            for other_event in category:
+                if other_event.lower() in message.lower():
+                    callback_clicked_other(payload='OTHER_' + other_event, event=event, data=data, raw_data=raw_data,
+                                           page=page)
+
+                    message_handled = 1
+
+        if message_handled == 1:
+            return
+
+        # For displaying department events
+        for dept_idx, dept in enumerate(data[1][data[0].index('technical')], 0):
+            dept_list = dept.split('/')
+            dept_list.append(raw_data['technical'][dept_idx]['alis'])
+            if dept == 'ec/el':
+                dept_list = ['electronics', 'communication']
+            if dept == 'computer/it':
+                dept_list = ['computer', 'information technology']
+
+            for mdept in dept_list:
+                if mdept.lower() in message.lower():
+                    callback_picked_dept(payload='PICK_' + dept, event=event, data=data, raw_data=raw_data, page=page)
+                    return
+
+        # For displaying info related to categories
+        for category in data[0]:
+            if category.lower() in message.lower():
+                if category.lower() == 'technical' and 'non' in message.lower():
+                    callback_picked_category(payload='CATEGORY__non-technical', event=event, data=data,
+                                             raw_data=raw_data,
+                                             page=page)
+                else:
+                    callback_picked_category(payload='CATEGORY__' + category, event=event, data=data, raw_data=raw_data,
+                                             page=page)
+                return
+        non_tech_keywords = ['non-tech', 'nontech']
+        for non_tech_keyword in non_tech_keywords:
+            if non_tech_keyword in message.lower():
+                callback_picked_category(payload='CATEGORY__non-technical', event=event, data=data, raw_data=raw_data,
+                                         page=page)
+                return
+        if 'tech' in message.lower():
+            callback_picked_category(payload='CATEGORY__technical', event=event, data=data, raw_data=raw_data,
+                                     page=page)
+            return
+
+        # Reach us
+        for keyword in reach_us_list:
+            if keyword.lower() in message.lower():
+                click_persistent_menu(payload='PMENU_' + 'map', event=event)
+                return
+
+        # Level 0 Menu
+        for keyword in categories_list:
+            if keyword.lower() in message.lower():
+                click_persistent_menu(payload='PMENU_' + 'Information', event=event)
+                return
+        if 'fine' in message.lower():
+            page.send(sender_id, 'Good')
+            return
+        bot_input = chatterbot.get_response(message)
+        if 'do you feel' in str(bot_input).lower():
+            page.send(sender_id, 'Good')
+        else:
+            print(bot_input)
+            page.send(sender_id, str(bot_input))
+    except Exception:
+        print('An error occured')
+        page.send(event.sender_id, 'Hello')
 
 @page.callback(['CATEGORY__(.+)'])
 def callback_picked_genre(payload, event):
