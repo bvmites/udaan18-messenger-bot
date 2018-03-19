747b30f819e3624a6b24814a3a469742d6b8bd1c
Pranshu Dave <pranshudave@gmail.com>
2018-03-19 15:31:26 +0530

    Add social media handles in CONFIG

--- CONFIG.py
+++ CONFIG.py
@@ -2,5 +2,9 @@ CONFIG = {
     'SERVER_URL': 'https://udaan18.com',
     'UDAAN_URL': 'https://lh3.googleusercontent.com/b6RDPkspjtpze1d6E_YfqHtLHEKAlYmOU33PAEKA1EW9moO0Kmcf5hkaWvQHZmIVhQ=w300',
     'BVM_LOGO': 'https://www.collegesearch.in/upload/institute/images/large/160426035818_1402992152php39qH8D.jpeg',
-    'BVM_ADDRESS': 'https://goo.gl/maps/V1voBqz8hW32'
+    'BVM_ADDRESS': 'https://goo.gl/maps/V1voBqz8hW32',
+    'UDAAN_WEBSITE': 'https://udaan18.com/',
+    'UDAAN_INSTA': 'https://instagram.com/teamudaan',
+    'UDAAN_TWITTER': 'https://twitter.com/teamudaan',
+    'UDAAN_GITHUB': 'https://github.com/bvmites'
 }
